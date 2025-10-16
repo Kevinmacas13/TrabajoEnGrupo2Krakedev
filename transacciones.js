0b0597735a4d0ee549bc55a672ae3e6125046d49
MichGuzm <michguzman2196@gmail.com>
2025-10-16 16:38:32 -0500

    deposito done

--- transacciones.js
+++ transacciones.js
@@ -1,13 +1,15 @@
-cuentas=[
+let cuentas=[
     {numeroCuenta:"02234567", cedula:"1714616123",nombre:"Juan",apellido:"Perez",saldo:0.0},
     {numeroCuenta:"02345211",cedula:"1281238233",nombre:"Felipe",apellido:"Caicedo",saldo:0.0}
 ]
+let esNuevo=false;
 
 cargar=function(){
     mostrarComponente("divTransacciones");
     ocultarComponente("divCuentas");
     ocultarComponente("divMovimientos");
-    
+    deshabilitarComponente("btnDepositar");
+    deshabilitarComponente("btnRetirar");
 }
 
 /*
@@ -15,19 +17,45 @@ cargar=function(){
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
 buscarCuenta=function(numeroCuenta){
-
+    let elementoCuenta;
+    let elementoCuentaEncontrada=null;
+    for(let i=0;i<cuentas.length;i++){
+        elementoCuenta=cuentas[i];
+        if(elementoCuenta.numeroCuenta==numeroCuenta){
+            elementoCuentaEncontrada=elementoCuenta;
+            break;
+        }
+    }
+    return elementoCuentaEncontrada;
 }
 
 ejecutarBusqueda=function(){
     //toma el numero de cuenta de la caja de texto
     //invoca a buscarCuenta y guarda el resultado en una variable
     //Si el resultado es diferente de null, muestra en pantalla, caso contrario muestra un alert
+    let cuentaBusqueda=recuperarTexto("txtCuentaExistente");
+    let cuentaEncontrada=buscarCuenta(cuentaBusqueda);
+    if(cuentaEncontrada==null){
+        alert("NUMERO DE CUENTA NO EXISTE");
+    }else{
+       mostarInfo();
+    }
+    habilitarComponente("btnDepositar");
+    habilitarComponente("btnRetirar");
 }
-
+mostarInfo=function(){
+    mostrarTextoEnCaja("txtNumeroCuentaExistente",cuentaEncontrada.numeroCuenta);
+    mostrarTextoEnCaja("txtNombreCuenta",cuentaEncontrada.nombre+" "+cuentaEncontrada.apellido);
+    mostrarTextoEnCaja("txtCedulaCuenta",cuentaEncontrada.cedula);
+    mostrarTextoEnCaja("txtSaldoCuenta",cuentaEncontrada.saldo);
+}
 depositar=function(numeroCuenta,monto){
-    let cuentaAfectada;
-    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
+    let cuentaAfectada=buscarCuenta(numeroCuenta)
+    if(cuentaAfectada!=null){
+        cuentaAfectada.saldo=cuentaAfectada.saldo+monto;
+        return true;
+    }
+    return false;
 }
 
 ejecutarDeposito=function(){
@@ -36,12 +64,11 @@ ejecutarDeposito=function(){
     //invoca a depositar
     //Muestra un mensaje TRANSACCION EXITOSA
     //Muestra en pantalla el nuevo saldo de la cuenta
-}
-
-depositar=function(numeroCuenta,monto){
-    let cuentaAfectada;
-    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
+    let numeroCuenta=recuperarTexto("txtCuentaExistente");
+    let monto=recuperarFloat("txtMonto");
+    let resultado=depositar(numeroCuenta,monto);
+    alert("TRANSACCION EXITOSA");
+    mostarInfo();
 }
 
 retirar=function(numeroCuenta,monto){
@@ -51,4 +78,12 @@ retirar=function(numeroCuenta,monto){
     //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
     //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
     //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
+}
+
+ejecutarRetiro=function(){
+    //Toma el numero de cuenta ingresado en la caja de texto
+    //Toma el monto ingresado en la caja de texto
+    //invoca a depositar
+    //Muestra un mensaje TRANSACCION EXITOSA
+    //Muestra en pantalla el nuevo saldo de la cuenta
 }
